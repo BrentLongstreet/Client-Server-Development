95f81036f83a008211fe228a279631158769cbe1
Brent Longstreet <70178454+BrentLongstreet@users.noreply.github.com>
2025-04-19 18:43:45 -0400

    Add files via upload
    Added Project Files

---
+++ animalShelter.py
@@ -0,0 +1,70 @@
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+
+class AnimalShelter(object):
+    """ CRUD operations for Animal collection in MongoDB """
+
+    def __init__(self, userName, passWord):
+        # Initializing the MongoClient. This helps to 
+        # access the MongoDB databases and collections.
+        # This is hard-wired to use the aac database, the 
+        # animals collection, and the aac user.
+        # Definitions of the connection string variables are
+        # unique to the individual Apporto environment.
+        #
+        # You must edit the connection variables below to reflect
+        # your own instance of MongoDB!
+        #
+        # Connection Variables
+        #
+        USER = userName
+        PASS = passWord
+        HOST = 'nv-desktop-services.apporto.com'
+        PORT = 33825
+        DB = 'AAC'
+        COL = 'animals'
+        #
+        # Initialize Connection
+        #
+        self.client = MongoClient('mongodb://%s:%s@%s:%d' % (USER,PASS,HOST,PORT))
+        self.database = self.client['%s' % (DB)]
+        self.collection = self.database['%s' % (COL)]
+
+# Complete this create method to implement the C in CRUD.
+    def create(self, data):
+        if data is not None:
+            insertData = self.database.animals.insert(data)  # data should be dictionary
+            if insertData != 0:
+                return True
+            else:
+                return False
+        else:
+            raise Exception("Nothing to save, because data parameter is empty")
+
+# Create method to implement the R in CRUD.
+    def read(self, searchData):
+        if searchData is not None:
+            data = self.database.animals.find(searchData, {"_id" : False})
+     # for document in data:
+          #  print(document)
+
+        else:
+            data = self.database.animals.find({}, {"_id" : False})
+
+        return data
+        
+    # Create method to implement the U in CRUD
+    def update(self, searchData, updateData):
+        if searchData is not None:
+            result = self.database.animals.update_many(searchData, {"$set": updateData})
+        else:
+            return "{}"
+        return result.raw_result
+    
+    # Create method to implement the D in CRUD
+    def delete(self, deleteData):
+        if deleteData is not None:
+            result = self.database.animals.delete_many(deleteData)
+        else:
+            return "{}"
+        return result.raw_result
